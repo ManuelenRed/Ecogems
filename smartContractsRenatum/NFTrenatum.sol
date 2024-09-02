87c208213295a2d2c559b57b2d0269696f5f14f1
Manuel En Red <116132636+ManuelenRed@users.noreply.github.com>
2024-09-01 23:22:42 -0500

    Arreglos del contrato
    Probado en Remix, listo para desplegar con hardhat

--- smartContractsRenatum/NFTrenatum.sol
+++ smartContractsRenatum/NFTrenatum.sol
@@ -1,60 +1,42 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract MyNFT is ERC721, ERC721Pausable, Ownable, ERC721Burnable, ERC721Enumerable {
+contract MyToken is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
-    mapping(uint256 => string) private _tokenURIs;
 
     constructor(address initialOwner)
-        ERC721("MyNFT", "MNFT")
+        ERC721("Jaguar", "JGR")
         Ownable(initialOwner)
     {}
 
-    // Pausable functions
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
-    // Mint new NFT
-    function safeMint(address to, string memory tokenURI_) public onlyOwner {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI_);
+        _setTokenURI(tokenId, uri);
     }
 
-    // Transfer NFT
-    function transferNFT(address from, address to, uint256 tokenId) public onlyOwner {
-        safeTransferFrom(from, to, tokenId);
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 
-    // Internal function to set token URI
-    function _setTokenURI(uint256 tokenId, string memory tokenURI_) internal {
-        require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
-        _tokenURIs[tokenId] = tokenURI_;
-    }
-
-    // Override the function to get token URI
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return _tokenURIs[tokenId];
-    }
-
-    // Required for ERC721Enumerable
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Pausable, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-    
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 }
